a6c0ad9bc11b5cc8808c5b9532b830261f4ffc8c
jovencio neto <151071159+JovencioNeto@users.noreply.github.com>
2024-08-28 16:25:35 -0300

    Add files via upload

---
+++ src/components/Character.js
@@ -0,0 +1,24 @@
+class Character{
+    #life
+    constructor(name){
+        this.name = name
+        this.#life = 1
+        this.maxLife = 1
+        this.attack = 0
+        this.defense = 0
+    }
+
+    // para pegar a vida char
+    get life(){
+        return this.life
+    }
+
+    // para enviar a nova vida do char
+
+    set life(newLife){
+            // verificação para que a vida não fique inferior a 0
+        this.#life = newLife < 0 ? 0 : newLife
+    }
+}
+
+module.exports = Character
---
+++ src/js/script.js
@@ -0,0 +1,2 @@
+let char = new Knight('Jimmy Neutron')
+let monster = new LittleMonster()
